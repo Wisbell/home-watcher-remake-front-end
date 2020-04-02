699bd9431b0ae7f2f0368c7956dd4794fedfab2e
West <coolwest@gmail.com>
2020-04-01 21:34:24 -0500

    Create incident service and module

--- src/app/incident/incident.module.ts
+++ src/app/incident/incident.module.ts
@@ -3,13 +3,20 @@ import { CommonModule } from '@angular/common';
 import { IncidentCreateComponent } from './incident-create/incident-create.component';
 import { IncidentEditComponent } from './incident-edit/incident-edit.component';
 import { IncidentListComponent } from './incident-list/incident-list.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
   declarations: [IncidentCreateComponent, IncidentEditComponent, IncidentListComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule,
+    MDBBootstrapModule.forRoot(),
   ]
 })
 export class IncidentModule { }
