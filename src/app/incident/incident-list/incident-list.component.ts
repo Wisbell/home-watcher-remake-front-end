04b5c7d148259d14f0c849dabd21f2c148c26656
West <coolwest@gmail.com>
2020-04-01 21:33:43 -0500

    Add incident list component

--- src/app/incident/incident-list/incident-list.component.ts
+++ src/app/incident/incident-list/incident-list.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormBuilder } from '@angular/forms';
+import { IncidentService } from '../incident.service';
+// import { Incident } from '../incident.model';
+import { IncidentDto } from '../incident.dto';
 
 @Component({
   selector: 'app-incident-list',
@@ -7,9 +12,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IncidentListComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public formBuilder: FormBuilder,
+    private incidentService: IncidentService,
+  ) { }
+
+  incidents: IncidentDto[];
 
   ngOnInit(): void {
+    this.getAllIncidents();
   }
 
+  getAllIncidents() {
+    this.incidentService.getAll()
+    .then( (data) => {
+      console.log('data', data);
+      const theIncidents = data as IncidentDto[];
+      this.incidents = theIncidents;
+    });
+  }
+
+  async deleteIncident(id: string) {
+    var result = confirm("Are you sure you want to delete this incident?");
+    if (result) {
+      await this.incidentService.delete(id);
+      this.getAllIncidents();
+    }
+  }
 }
