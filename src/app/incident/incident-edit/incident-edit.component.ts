b985d11fc5f81546f460f33050a2c487d18f6493
West <coolwest@gmail.com>
2020-04-01 21:32:49 -0500

    Add incident edit form

--- src/app/incident/incident-edit/incident-edit.component.ts
+++ src/app/incident/incident-edit/incident-edit.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { IncidentService } from '../incident.service';
+import { IncidentDto } from '../incident.dto';
 
 @Component({
   selector: 'app-incident-edit',
@@ -7,9 +12,69 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IncidentEditComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private httpClient: HttpClient,
+    private route: ActivatedRoute,
+    private router: Router,
+    public formBuilder: FormBuilder,
+    private incidentService: IncidentService
+  ) { }
+
+  private incident: IncidentDto;
+  private incidentId: string;
+  private getIncidentEndpoint: string = 'http://localhost:3000/incidents';
+
+  public incidentForm: FormGroup = this.formBuilder.group({
+    id: new FormControl({ value: '', disabled: true }),
+    dateCreated: new FormControl({ value: '', disabled: true}),
+    imageId: new FormControl({ value: '', disabled: true}),
+    text: new FormControl(),
+  })
 
   ngOnInit(): void {
+    this.incidentId = this.route.snapshot.paramMap.get('id');
+    this.getIncidentEndpoint = `${this.getIncidentEndpoint}/${this.incidentId}`
+    this.setUp();
   }
 
+  async onSubmit(form: FormGroup) {
+    if (!form.valid) {
+      return;
+    }
+
+    const { id, dateCreated, imageId, text } = form.getRawValue();
+    const incidentToUpdate: IncidentDto = new IncidentDto(
+      id, text, dateCreated, imageId
+    );
+
+    await this.incidentService.update(
+      this.incident.id,
+      incidentToUpdate
+    );
+
+    this.router.navigate(['/incidents']);
+  }
+
+  async getIncident() {
+    await this.incidentService.get(this.incidentId)
+    .then( (data) => {
+      const theIncident = data as IncidentDto;
+      this.incident = theIncident;
+    });
+  }
+
+  async setUp() {
+    await this.getIncident();
+    const { id, dateCreated, text, imageId } = this.incident;
+    this.setUserForm(new IncidentDto(id, text, dateCreated, imageId));
+  }
+
+  setUserForm(incidentDto: IncidentDto) {
+    this.incidentForm.setValue({
+      id: incidentDto.id,
+      dateCreated: incidentDto.dateCreated,
+      text: incidentDto.text,
+      imageId: incidentDto.imageId,
+    })
+  }
 }
