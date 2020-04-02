aaf35612891fbc56e8466fa981fd645b690e9c66
West <coolwest@gmail.com>
2020-04-01 21:32:40 -0500

    Add incident create form

--- src/app/incident/incident-create/incident-create.component.ts
+++ src/app/incident/incident-create/incident-create.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { IncidentService } from '../incident.service';
+import { HttpClient } from '@angular/common/http';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { Incident } from '../incident.model';
+import { SecurityService } from '../../security/security.service';
+import { Image } from '../../security/image.model';
+import { IncidentDto } from '../incident.dto';
 
 @Component({
   selector: 'app-incident-create',
@@ -7,9 +15,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IncidentCreateComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    public formBuilder: FormBuilder,
+    private incidentService: IncidentService,
+    private securityService: SecurityService,
+  ) { }
+
+  public imageId: string;
+  public incidentImage: Image;
+
+  public incidentForm: FormGroup = this.formBuilder.group({
+    text: new FormControl()
+  })
 
   ngOnInit(): void {
+    this.setUp();
   }
 
+  async onSubmit(form: FormGroup) {
+    if (!form.valid) {
+      return;
+    }
+
+    const { text } = form.getRawValue();
+
+    const newIncident: IncidentDto = new IncidentDto(text, this.imageId);
+
+    const savedIncident = await this.incidentService.create(newIncident);
+
+    console.log('savedIncident', savedIncident);
+
+    this.router.navigate(['/incidents']);
+  }
+
+  async getImage() {
+    await this.securityService.getImage(this.imageId)
+    .then( (data) => {
+      const theImage = data as Image;
+      this.incidentImage = theImage;
+    });
+  }
+
+  async setUp() {
+    this.imageId = this.route.snapshot.paramMap.get('id');
+    this.getImage();
+  }
 }
