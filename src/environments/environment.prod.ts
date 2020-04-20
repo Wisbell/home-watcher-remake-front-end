5f8552a8aa1eb06951111394f828ca7f3a6312d2
West <coolwest@gmail.com>
2020-04-20 12:22:24 -0500

    Add commentary.  Add new ngrok url as rasp pi url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,8 @@
 export const environment = {
   production: true,
   apiUrl: 'https://home-watcher-api.herokuapp.com',
-  raspPiUrl: 'http://home-watcher.serverless.social'
+  // NOTE: http will not work as intended due to lack of security
+  // raspPiUrl: 'http://home-watcher.serverless.social',
+  // TODO: Add ability to update rasp pi url during run time
+  raspPiUrl: 'https://42d38b30.ngrok.io',
 };
