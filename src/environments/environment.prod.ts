76e70b70908499fdf59579129c534c327990542b
West <coolwest@gmail.com>
2020-04-21 16:45:51 -0500

    Add to comment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: 'https://home-watcher-api.herokuapp.com',
-  // NOTE: http will not work as intended due to lack of security
+  // NOTE: http will not work as intended due to lack of security (https ssl)
   // raspPiUrl: 'http://home-watcher.serverless.social',
   // TODO: Add ability to update rasp pi url during run time
   raspPiUrl: 'https://42d38b30.ngrok.io',
